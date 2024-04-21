55b5f8e1d11d9f2e81d9deb0d79008894b7ef453
AdrianGerman <adrian.german1019@gmail.com>
2024-04-21 06:54:16 -0600

    connect HTML with JavaScript

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function seleccionarMAscotaJugador() {
+  alert("SELECCIONASTE A TU MASCOTA");
+}
+
+let botonMascotaJugador = document.getElementById("boton-mascota");
+botonMascotaJugador.addEventListener("click", seleccionarMAscotaJugador);
