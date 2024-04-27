86fe0dff4eb7dec928658cc2e44e241e83fe1a6f
AdrianGerman <adrian.german1019@gmail.com>
2024-04-27 11:26:56 -0600

    movement in all directions

--- js/main.js
+++ js/main.js
@@ -49,6 +49,7 @@ let victoriasEnemigo = 0;
 let vidasJugador = 3;
 let vidasEnemigo = 3;
 let lienzo = mapa.getContext("2d");
+let intervalo;
 
 class Mokepon {
   constructor(nombre, foto, vida) {
@@ -62,6 +63,8 @@ class Mokepon {
     this.alto = 80;
     this.mapaFoto = new Image();
     this.mapaFoto.src = foto;
+    this.velocidadX = 0;
+    this.velocidadY = 0;
   }
 }
 
@@ -122,6 +125,7 @@ function seleccionarMascotaJugador() {
   sectionSeleccionarMascota.style.display = "none";
   // sectionSeleccionarAtaque.style.display = "flex";
   sectionVerMapa.style.display = "flex";
+  intervalo = setInterval(pintarPersonaje, 50);
 
   if (inputHipodoge.checked) {
     spanMascotaJugador.innerHTML = inputHipodoge.id;
@@ -286,13 +290,31 @@ function aleatorio(min, max) {
 }
 
 function pintarPersonaje() {
+  capipepo.x = capipepo.x + capipepo.velocidadX;
+  capipepo.y = capipepo.y + capipepo.velocidadY;
   lienzo.clearRect(0, 0, mapa.width, mapa.height);
   lienzo.drawImage(capipepo.mapaFoto, capipepo.x, capipepo.y, capipepo.ancho, capipepo.alto);
 }
 
-function moverCapipepo() {
-  capipepo.x = capipepo.x + 5;
-  pintarPersonaje();
+function moverDerecha() {
+  capipepo.velocidadX = 5;
+}
+
+function moverIzquierda() {
+  capipepo.velocidadX = -5;
+}
+
+function moverAbajo() {
+  capipepo.velocidadY = 5;
+}
+
+function moverArriba() {
+  capipepo.velocidadY = -5;
+}
+
+function detenerMovimiento() {
+  capipepo.velocidadX = 0;
+  capipepo.velocidadY = 0;
 }
 
 window.addEventListener("load", iniciarJuego);
