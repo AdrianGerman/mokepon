918f280a0c08b32b71d02253ab7bfba81f933b71
AdrianGerman <adrian.german1019@gmail.com>
2024-04-29 13:32:04 -0600

    optimization improvements

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ const mapa = document.getElementById("mapa");
 
 let jugadorId = null;
 let mokepones = [];
+let mokeponesEnemigos = [];
 let ataqueJugador = [];
 let ataqueEnemigo = [];
 let opcionDeMokepones;
@@ -364,9 +365,9 @@ function pintarCanvas() {
   lienzo.drawImage(mapaBackground, 0, 0, mapa.width, mapa.height);
   mascotaJugadorObjeto.pintarMokepon();
   enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y);
-  hipodogeEnemigo.pintarMokepon();
-  capipepoEnemigo.pintarMokepon();
-  ratigueyaEnemigo.pintarMokepon();
+  mokeponesEnemigos.forEach(function (mokepon) {
+    mokepon.pintarMokepon();
+  });
   if (mascotaJugadorObjeto.velocidadX !== 0 || mascotaJugadorObjeto.velocidadY !== 0) {
     revisarColision(hipodogeEnemigo);
     revisarColision(capipepoEnemigo);
@@ -388,7 +389,7 @@ function enviarPosicion(x, y) {
     if (res.ok) {
       res.json().then(function ({ enemigos }) {
         console.log(enemigos);
-        enemigos.forEach(function (enemigo) {
+        mokeponesEnemigos = enemigos.map(function (enemigo) {
           let mokeponEnemigo = null;
           const mokeponNombre = enemigo.mokepon.nombre || "";
           if (mokeponNombre === "Hipodoge") {
@@ -417,7 +418,7 @@ function enviarPosicion(x, y) {
           mokeponEnemigo.x = enemigo.x;
           mokeponEnemigo.y = enemigo.y;
 
-          mokeponEnemigo.pintarMokepon();
+          return mokeponEnemigo;
         });
       });
     }
