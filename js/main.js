278645328416660d43ca7a9c123ebf3a4839ed47
AdrianGerman <adrian.german1019@gmail.com>
2024-04-30 09:45:16 -0600

    consuming enemy attacks API

--- js/main.js
+++ js/main.js
@@ -274,6 +274,20 @@ function enviarAtaques() {
       ataques: ataqueJugador,
     }),
   });
+  intervalo = setInterval(obtenerAtaques, 50);
+}
+
+function obtenerAtaques() {
+  fetch(`http://localhost:8080/mokepon/${enemigoId}/ataques`).then(function (res) {
+    if (res.ok) {
+      res.json().then(function ({ ataques }) {
+        if (ataques.length === 5) {
+          ataqueEnemigo = ataques;
+          combate();
+        }
+      });
+    }
+  });
 }
 
 function seleccionarMascotaEnemigo(enemigo) {
@@ -309,6 +323,7 @@ function indexAmbosOponentes(jugador, enemigo) {
 }
 
 function combate() {
+  clearInterval(intervalo);
   for (let index = 0; index < ataqueJugador.length; index++) {
     if (ataqueJugador[index] === ataqueEnemigo[index]) {
       indexAmbosOponentes(index, index);
